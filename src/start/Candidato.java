ce04ba425fcc91effdc57457bcbbf8300887737b
JoaoVictorK <joaozin10k@gmail.com>
2024-11-13 18:43:51 -0300

    iniciando o projeto

---
+++ src/start/Candidato.java
@@ -0,0 +1,22 @@
+package start;
+
+public class Candidato {
+    private String Nome;
+    private int id;
+
+    public String getNome() {
+        return Nome;
+    }
+
+    public void setNome(String nome) {
+        Nome = nome;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+}
